1f1341e32b4464e842591952f46de27085c3d3ef
Yiming Chen <yxc1103@case.edu>
2019-04-19 00:51:42 -0400

    Create lc141.java

---
+++ Jerry/Linkedlist/lc141.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) {
+            return false;
+        }
+        
+        ListNode fast = head, slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
+    /**
+     * Result
+     * Runtime: faster than 100.00%
+     * Memory: less than 39.98%
+     */
+}
