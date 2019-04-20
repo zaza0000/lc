362f2b56606399da076078190026f371b836666e
Yiming Chen <yxc1103@case.edu>
2019-04-20 00:11:56 -0400

    Create lc142.java

---
+++ Jerry/Linkedlist/lc142.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        
+        Set<ListNode> seen = new HashSet<>();
+        ListNode curr = head;
+        while (curr != null) {
+            if (seen.contains(curr)) {
+                return curr;
+            }
+            seen.add(curr);
+            curr = curr.next;
+        }
+        
+        return null;
+    }
+
+    /**
+     * Result
+     * Runtime: faster than 21.20%
+     * Memory: less than 82.82%
+     */
+}
