51053fb33b8866c20f3a46aed7b71d199cab378c
Yiming Chen <yxc1103@case.edu>
2019-04-22 17:44:51 -0400

    Create lc17.java

---
+++ Jerry/DPS+Backtrack/lc17.java
@@ -0,0 +1,43 @@
+class Solution {
+    public final static Map<Character, List<Character>> map = new HashMap(){{
+        put('2', Arrays.asList('a','b','c'));
+        put('3', Arrays.asList('d','e','f'));
+        put('4', Arrays.asList('g','h','i'));
+        put('5', Arrays.asList('j','k','l'));
+        put('6', Arrays.asList('m','n','o'));
+        put('7', Arrays.asList('p','q','r','s'));
+        put('8', Arrays.asList('t','u','v'));
+        put('9', Arrays.asList('w','x','y','z'));
+    }};
+    
+    
+    public List<String> letterCombinations(String digits) {
+        if (digits == null) {
+            return null;
+        }
+        
+        List<String> result = new ArrayList<>();
+        if (digits.length() == 0) {
+            return result;
+        }
+        
+        Character first = digits.charAt(0);
+        List<String> subList = letterCombinations(digits.substring(1));
+        for (Character c: map.get(first)) {
+            if (subList.size() == 0) {
+                subList.add("");
+            }
+            for (String str: subList) {
+                result.add(c + str);
+            }
+        }
+        
+        return result;
+    }
+
+    /**
+     * Result
+     * Runtime: faster than 89.34%
+     * Memory: less than 75.17%
+     */
+}
